4a6b049323c4c2458f16d220e2f31c88cb6d7407
Night0880 <luismartz199@gmail.com>
2020-07-28 07:27:48 -0430

    Separating everything into several files (as it is not much only in 2).

--- main.go
+++ main.go
@@ -2,18 +2,13 @@ package main //package main (main file)
 
 import (
 	"context"       //package context
-	"encoding/json" //package encoding/json
 	"fmt"           //package fmt
-	"io/ioutil"     //package io/ioutil
 	"log"           //package log
 	"strconv"       //package strconv
 	"strings"       //package strings
 	"time"          //package time
-	"database/sql"  //package database/sql
-   "errors"		 	 //package errors
 
 	"github.com/andersfylling/disgord" //lib disgord
-	_ "github.com/mattn/go-sqlite3"	  //driver sqlite3
 
 )
 
@@ -27,9 +22,6 @@ var (
 	prefix string  //variable where the prefix will be stored
 
 	ctx = context.Background() //var ctx by context.Background() (Context)
-
-	db *sql.DB //variable where the connection to the database will be stored
-
 )
 
 func main() {
@@ -463,223 +455,4 @@ func messageCreate(session disgord.Session, m *disgord.MessageCreate) {
 		break
 	}
 
-}
-
-/*--------------- List ALL Permissions -------------------*/
-var (
-	CREATE_INSTANT_INVITE = disgord.PermissionCreateInstantInvite
-	KICK_MEMBERS          = disgord.PermissionKickMembers
-	BAN_MEMBERS           = disgord.PermissionBanMembers
-	ADMINISTRATOR         = disgord.PermissionAdministrator
-	MANAGE_CHANNELS       = disgord.PermissionManageChannels
-	MANAGE_GUILD          = disgord.PermissionManageServer
-	ADD_REACTIONS         = disgord.PermissionAddReactions
-	VIEW_AUDIT_LOG        = disgord.PermissionViewAuditLogs
-	VIEW_CHANNEL          = disgord.PermissionReadMessages
-	SEND_MESSAGES         = disgord.PermissionSendMessages
-	SEND_TTS_MESSAGES     = disgord.PermissionSendTTSMessages
-	MANAGE_MESSAGES       = disgord.PermissionManageMessages
-	EMBED_LINKS           = disgord.PermissionEmbedLinks
-	ATTACH_FILES          = disgord.PermissionAttachFiles
-	READ_MESSAGE_HISTORY  = disgord.PermissionReadMessageHistory
-	MENTION_EVERYONE      = disgord.PermissionMentionEveryone
-	USE_EXTERNAL_EMOJIS   = disgord.PermissionUseExternalEmojis
-	CONNECT               = disgord.PermissionVoiceConnect
-	SPEAK                 = disgord.PermissionVoiceSpeak
-	MUTE_MEMBERS          = disgord.PermissionVoiceMuteMembers
-	DEAFEN_MEMBERS        = disgord.PermissionVoiceDeafenMembers
-	MOVE_MEMBERS          = disgord.PermissionVoiceMoveMembers
-	USE_VAD               = disgord.PermissionVoiceUseVAD
-	PRIORITY_SPEAKER      = disgord.PermissionVoicePrioritySpeaker
-	CHANGE_NICKNAME       = disgord.PermissionChangeNickname
-	MANAGE_NICKNAMES      = disgord.PermissionManageNicknames
-	MANAGE_ROLES          = disgord.PermissionManageRoles
-	MANAGE_WEBHOOKS       = disgord.PermissionManageWebhooks
-	MANAGE_EMOJIS         = disgord.PermissionManageEmojis
-)
-
-/*-------------------- Verify Permissions --------------------*/
-func hasPermission(session disgord.Session, User *disgord.User, GuildID disgord.Snowflake, Permission uint64) bool {
-	
-	Member, err := session.GetMember(ctx, GuildID, User.ID, disgord.IgnoreCache)
-	
-	if err != nil {
-		//If an error occurs when trying to get the user as a member we return false and the error to the console
-		log.Print(err)
-		return false
-	}
-
-	for _, roleID := range Member.Roles {
-		guild, err := session.GetGuild(ctx, GuildID, disgord.IgnoreCache)
-		if err != nil {
-			//If an error occurs when trying to get the server we return false and the error to the console
-			log.Print(err)
-			return false
-		}
-
-		if guild.OwnerID == Member.UserID {
-			return true //If the user ID is equal to the owner's id, we omit the owner of the server (since it has all the permissions)
-		}
-
-		role, err := guild.Role(roleID)
-		if err != nil {
-			//If an error occurs when trying to obtain the Role, we return false and the error to the console
-			log.Print(err)
-			return false
-		}
-
-		if (role.Permissions & 0x8) == 0x8 {
-			return true //If you have administrator permission we omit it (since with administrator permission it is to have all the permissions)
-
-		}
-
-		if (role.Permissions & Permission) == Permission {
-			//If you have the indicated permission we return true
-			return true
-		}
-	}
-	return false //If you do not own the server, you do not have administrator permission and you do not have the permission to search, then we return false
-}
-
-/*-------------------- Config data --------------------*/
-
-type configFile struct {
-	Prefix   string `json:"Prefix"`
-	BotID    string `json:"Bot_ID,omitempty"`
-	BotToken string `json:"Bot_Token"`
-}
-
-/* config.json
- * "Prefix":"Prefix" ->          the bot prefix
- * "Bot_ID":"BOT-ID" ->          The Bot ID
- * "Bot_Token":"BOT-TOKEN" ->    The bot token (https://discord.com/developers/applications)
- */
-
-func loadConfig(filename string) *configFile {
-	/* Read file config.json return Type Config */
-	body, err := ioutil.ReadFile(filename)
-
-	if err != nil {
-		log.Print("Error loading config ", err)
-		return nil
-	}
-
-	var config configFile
-	/* var config Type Config */
-	json.Unmarshal(body, &config)
-
-	return &config
-}
-
-
-/*-------------------- Database --------------------*/
-
-/* Create the default server data only if it does not exist */
-func Create(id disgord.Snowflake, prefix string) error{
-	db := GetConnection() //Connection to the database
-
-	query := `INSERT INTO settings (GuildId, Prefix)
-					VALUES(?,?)` //Sql statement
- 
-	stmt, err := db.Prepare(query) //queries prepared
-	if err != nil{
-		 return err //If an error occurs we will return it
-	}
-	defer stmt.Close() //We make sure to close the query prepared at the end of the function execution
-
-	rows, err := stmt.Exec(id, prefix) //We execute
-	if err != nil{ //If an error occurs while inserting data, we return the error
-		 return err
-	}
-
-	if i, err := rows.RowsAffected(); err != nil || i != 1{
-		//If an error occurs or the affected rows were 0, we return this error
-		 return errors.New("ERROR: An affected row was expected")
-	}
-
-	return nil //At this point, if it gets here everything went well so we returned nil
-}
-
-/* Get the prefix from the database */
-func GetPrefix(gId disgord.Snowflake) string{
-	db := GetConnection() //Connection to the database
-	var Dataprefix string //var Dataprefix
-	query := `SELECT prefix FROM settings WHERE GuildId = ?` //Sql statement
-
-	err := db.QueryRow(query, gId).Scan(&Dataprefix) //We execute the query and pass the required parameter (server id)
-	if err != nil{
-		//If an error occurs it can usually be because I did not find a row linked to the server id,
-		// so I inserted new data
-		
-		 _ = Create(gId, config.Prefix)
-		 Dataprefix = config.Prefix
-		 /* As we insert the default prefix (the one that is in the config.json) 
-		 we will return that prefix and for the next query it will be obtaining 
-		 the prefix from the database */
-	}
-
-	return Dataprefix //There will always be a returned prefix
-}
-
-/*--------- Prefix update ---------*/
-func UpdatePrefix(id disgord.Snowflake, newPrefix string) error{
-	db := GetConnection() //Connection to the database
-	query := `UPDATE settings SET Prefix = ? WHERE GuildId = ?` //Sql statement
-	stmt, err := db.Prepare(query) //Preparing the sentence
-	if err != nil{
-		//If an error occurs we will return it
-		 return err
-	}
-	
-	defer stmt.Close() //we ensure that you close the query at the end
-
-	row, err := stmt.Exec(newPrefix, id)//We pass the necessary parameters for the shift consultation
-	if err != nil{
-		/* If an error occurs normally it is that I did not find the row linked to the server id, so we insert the data but with the new prefix */
-		 Create(id, newPrefix)
-		 return nil
-	}
-
-	if i, err := row.RowsAffected(); err != nil || i != 1{
-		//If an error occurs or the affected rows were 0, we return this error
-		 return errors.New("ERROR: An affected row was expected")
-	}
-
-
-	return nil //At this point, if it gets here everything went well so we returned nil
-}
-
-func GetConnection() *sql.DB { //Connection to the database
-	if db != nil { //if there is already an open connection to the database we return that connection
-		return db
-	}
-
-	var err error
-	db, err = sql.Open("sqlite3", "database.db")
-	//driver: sqlite3
-	//databasename: database.db
-
-	if err != nil {
-		panic(err) //If an error occurs while opening the connection to the database
-	}
-	return db //We return the connection
-}
-
-
-// Create db tables if they don't exist
-func CreateTableIfNotExist() error { 
-	 db := GetConnection() //Connection to the database
-	 
-    query := `CREATE TABLE IF NOT EXISTS settings (
-        		GuildId INTEGER PRIMARY KEY NOT NULL,
-        		Prefix TEXT
-				 );`//sql statement
-				 
-	_, err := db.Exec(query) //execution of sentence
-   if err != nil{
-		//if an error occurs while inserting the data, we will return it (normally it may be because there is no connection to the database)
-      return err
-    }
-
-    return nil //At this point, if it gets here everything went well so we returned nil
 }
